974b3d3dbc5036b5114a7ad1a481bcc8e1dca3ed
ZerrickStaples <zerrick.staples@gmail.com>
2017-12-16 04:12:19 -0500

    Added styling to application.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import Gift from '../components/Gift';
+import { max_numbers, max_number } from '../helper';
 
 class App extends Component {
 
@@ -16,7 +17,7 @@ class App extends Component {
 
         const ids = this.state.gifts.map(gift => gift.id);
 
-        const max_id = ids.length > 0 ? Math.max(...ids) : 0;
+        const max_id = max_number(ids);
 
         gifts.push({ id: max_id+1 });
 
